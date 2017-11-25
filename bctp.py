4bc718d25b3e69ab68022224d1c32cd94f95726e
mkoch <mkoch@centos.oben>
2017-11-25 16:43:36 +0100

    fixed encoding problems

--- bctp.py
+++ bctp.py
@@ -36,7 +36,6 @@ for thread in config['threads']:
             + "[" + entry.username + "|" + entry.userrank + "]\n" \
             + entry.content
             db[entry.id] = "1"
-
             bot.send_message(chat_id=my_chat_id, text=message_text, parse_mode=telegram.ParseMode.MARKDOWN)
             time.sleep(0.05) # spam threshold
 
