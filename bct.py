dedf33ec632682784dd3456c520619e17b07980d
Matthias Koch <matthiaskoch@Matthiass-MacBook-Pro.local>
2017-11-25 15:48:59 +0100

    first commit

---
+++ bct.py
@@ -0,0 +1,61 @@
+import requests
+from lxml import html, etree
+import html2text
+import hashlib
+
+
+class BctEntry:
+    def __init__(self, id, username, userrank, content):
+        self.id = id;
+        self.username = username;
+        self.userrank = userrank;
+        self.content = content;
+
+
+def _get_html2text():
+    h2t = html2text.HTML2Text()
+    h2t.ignore_images = True
+    h2t.ignore_links = True
+    return h2t
+
+def _read_url(url):
+    result = requests.get(url, headers = {'User-agent': 'cc1'})
+    if (result):
+        return result.content
+    return False
+
+def _get_link_for_last_page_of_thread(forum_thread):
+    tree = html.fromstring(forum_thread)
+    link_to_last_page_path = "//div[@id='bodyarea']/table[1]//td[1]/a[last()]"
+    return tree.xpath(link_to_last_page_path)[0].get("href")
+
+def _get_entries_for_thread_page(forum_thread_page):
+    result = requests.get(forum_thread_page, headers = {'User-agent': 'cc1'})
+    tree = html.fromstring(result.content)
+    entries_path = "//form[@id='quickModForm']//td[@class='windowbg' or @class='windowbg2']/table"
+    return tree.xpath(entries_path)
+
+def _create_bctentry_from_html_subtree(subtree, h2t):
+    username =  subtree.find(".//td[@class='poster_info']/b/a").text
+    rank =  subtree.find(".//td[@class='poster_info']//div[@class='smalltext']").text.strip()
+    text =  subtree.find(".//td[@class='td_headerandpost']/div[@class='post']")
+    content =  etree.tostring(text).strip()
+    content = h2t.handle(content)
+    m = hashlib.md5()
+    m.update(etree.tostring(subtree))
+    id = m.digest()
+    return BctEntry(id, username, rank, content)
+
+def read_last_page(thread_url):
+    h2t = _get_html2text()
+
+    forum_html = _read_url(thread_url)
+    if forum_html:
+        link_to_last_page = _get_link_for_last_page_of_thread(forum_html)
+        entries = _get_entries_for_thread_page(link_to_last_page)
+        return [_create_bctentry_from_html_subtree(subtree, h2t) for subtree in entries]
+
+
+
+
+
---
+++ bctp.py
@@ -0,0 +1,43 @@
+import yaml
+import telegram
+import lsm
+import bct
+
+
+# read config
+def _read_config(filename):
+    stream = open(filename, "r")
+    config = yaml.load(stream)
+    return config
+
+
+def _get_telegram_bot(bot_key):
+    bot = telegram.Bot(token=bot_key)
+    return bot
+
+def _is_empty(entry):
+    return (not entry.content.strip() or entry.content.strip() == entry.username)
+
+config = _read_config("config.yaml")
+db = lsm.LSM(config["db"]["filename"])
+bot = _get_telegram_bot(config["telegram"]["bot_key"])
+my_chat_id = config["telegram"]["chat_id"]
+
+for thread in config['threads']:
+    (name,url) = thread.items()[0]
+    entries = bct.read_last_page(url)
+
+    for entry in entries:
+        if not entry.id in db and not _is_empty(entry):
+            print "= " + name + " ========="
+            print "[" + entry.username + "|" + entry.userrank + "]"
+            print entry.content
+            db[entry.id] = "1"
+
+
+#bot.send_message(chat_id=my_chat_id, text="test test test", parse_mode=telegram.ParseMode.MARKDOWN)
+
+
+
+
+
