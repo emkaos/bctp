857ea20f31431af44250d3c5a72d1f97379df3be
Matthias Koch <matthiaskoch@Matthiass-MacBook-Pro.local>
2017-11-25 16:21:02 +0100

    change sleep from 50 secs to 50ms

--- bctp.py
+++ bctp.py
@@ -38,7 +38,7 @@ for thread in config['threads']:
             db[entry.id] = "1"
 
             bot.send_message(chat_id=my_chat_id, text=message_text, parse_mode=telegram.ParseMode.MARKDOWN)
-            time.sleep(50) # spam threshold
+            time.sleep(0.05) # spam threshold
 
 
 
